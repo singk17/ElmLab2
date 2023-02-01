5d8a245d282b1799c015f741ed6f0fc1be4b03a9
KamS04 <singhkam07@gmail.com>
2023-02-01 16:32:05 -0500

    Basic Main Setup

--- src/Main.elm
+++ src/Main.elm
@@ -4,9 +4,9 @@ import Browser
 import GraphicSVG exposing (..)
 import GraphicSVG.App exposing (..)
 
-import Core
-
 import YOURNAME
 
-view model = model collage 192 128 YOURNAME.myShapes
-main = gameApp Core.Tick { model = Core.model, view = view, update = YOURNAME.update, title = "PUT SOME TITLE HERE" }
+type Msg = Tick Float GetKeyState
+
+view model = collage 192 128 (YOURNAME.shapes model)
+main = gameApp Tick { model = 0, view = view, update = (\x -> \y -> y), title = "PUT SOME TITLE HERE" }
